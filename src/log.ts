d71ab48628d492afb939213eb74b3ef8b7bceb42
Dean Pierce <pierce403@gmail.com>
2025-12-17 14:43:58 -0800

    Fix error logging serialization

--- src/log.ts
+++ src/log.ts
@@ -4,5 +4,8 @@ export const log = pino({
   level: process.env.LOG_LEVEL || 'info',
   base: undefined,
   timestamp: pino.stdTimeFunctions.isoTime,
+  serializers: {
+    err: pino.stdSerializers.err,
+    error: pino.stdSerializers.err,
+  },
 });
-
